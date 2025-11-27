e3fe2fd1d11ba551e0bc57790822e96d8e8d134b
Sylvester0456 <sylvesterchukwuahachie@gmail.com>
2025-11-27 10:33:06 +0100

    made changes

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path):
+    with open(file_path) as f:
+        requirements = f.readlines()
+    requirements = [req.strip() for req in requirements if req.strip()]
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name='ml2project',
+    author='Sylvester',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
